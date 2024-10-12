0b0b4dfc6b55d7cb34fa71a887bccf5cbe81a96c
Julian Diaz <julian.diaz@dev-codes.io>
2024-10-12 10:34:03 -0500

    First Commit

---
+++ src/main/java/co/edu/uco/talklate/TalklateApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.talklate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalklateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalklateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uco/talklate/TalklateApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.talklate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalklateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
