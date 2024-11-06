37637ed8e1e99e6a713ab7d310999a4eecfc8fe4
Julian Diaz <julian.diaz@dev-codes.io>
2024-11-05 22:00:06 -0500

    - ADD api responses

--- src/main/java/co/edu/uco/talklate/controller/AuthenticationController.java
+++ src/main/java/co/edu/uco/talklate/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import co.edu.uco.talklate.domain.auth.LoginRequest;
 import co.edu.uco.talklate.domain.auth.RegisterRequest;
 import co.edu.uco.talklate.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,12 +20,30 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping(value = "register")
-    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authenticationService.register(request));
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            AuthResponse authResponse = authenticationService.register(request);
+            if (authResponse != null) {
+                return ResponseEntity.ok(authResponse);
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Registration couldn't be completed");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
     @PostMapping(value = "login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(authenticationService.login(request));
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        try {
+            AuthResponse authResponse = authenticationService.login(request);
+            if (authResponse != null) {
+                return ResponseEntity.ok(authResponse);
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Couldn't login. Please check fields and try again");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 }
--- src/main/java/co/edu/uco/talklate/controller/TranslationController.java
+++ src/main/java/co/edu/uco/talklate/controller/TranslationController.java
@@ -4,6 +4,7 @@ import co.edu.uco.talklate.domain.translation.TranslationRequest;
 import co.edu.uco.talklate.domain.translation.TranslationResponse;
 import co.edu.uco.talklate.service.TranslationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,23 +19,59 @@ public class TranslationController {
     private final TranslationService translationService;
 
     @PostMapping(value = "create")
-    public ResponseEntity<TranslationResponse> createRequest(@RequestBody TranslationRequest translationRequest) {
-        return ResponseEntity.ok(translationService.createTranslation(translationRequest));
+    public ResponseEntity<?> createRequest(@RequestBody TranslationRequest translationRequest) {
+        try {
+            TranslationResponse translationResponse = translationService.createTranslation(translationRequest);
+            if (translationResponse != null) {
+                return ResponseEntity.ok(translationResponse);
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating request");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
     @GetMapping(value = "all")
-    public ResponseEntity<ArrayList<TranslationResponse>> getAllRequest() {
-        return ResponseEntity.ok(translationService.listAllTranslations());
+    public ResponseEntity<?> getAllRequest() {
+        try {
+            ArrayList<TranslationResponse> translationResponse = translationService.listAllTranslations();
+            if (translationResponse != null) {
+                return ResponseEntity.ok(translationResponse);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error getting request list");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
     @PostMapping(value = "update")
-    public ResponseEntity<TranslationResponse> updateRequest(@RequestBody TranslationRequest translationRequest) {
-        return ResponseEntity.ok(translationService.updateTranslation(translationRequest));
+    public ResponseEntity<?> updateRequest(@RequestBody TranslationRequest translationRequest) {
+        try {
+            TranslationResponse translationResponse = translationService.updateTranslation(translationRequest);
+            if (translationResponse != null) {
+                return ResponseEntity.ok(translationResponse);
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating request");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
     @DeleteMapping(value = "delete")
-    public ResponseEntity<Boolean> deleteRequest(UUID id) {
-        return ResponseEntity.ok(translationService.deleteTranslation(id));
+    public ResponseEntity<?> deleteRequest(UUID id) {
+        try {
+            Boolean response = translationService.deleteTranslation(id);
+            if (response) {
+                return ResponseEntity.ok(response);
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error deleting request");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
 }
